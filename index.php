64e5b91039b27988b9466217cf182d27c8668164
SvjetlanaPajic <138154800+SvjetlanaPajic@users.noreply.github.com>
2023-06-29 20:34:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ header("Content-type: application/vnd.ms-word");
+ header("Content-Disposition: attachment; Filename=yourcoolwordfile.doc");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Windows-1252">
+</head>
+<body>
+<h1>My Word File created with PHP</h1>
+<p>This is a text</p>
+<p>This is another paragraph.</p>
+</body>
